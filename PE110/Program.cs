44a1916b531a0e9ffa26041b600e99ecd49dec43
Berk Isaac <berk.isaac@gmail.com>
2018-11-10 22:16:34 -0500

    Added 110 and 187

--- PE110/Program.cs
+++ PE110/Program.cs
@@ -1,29 +1,108 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Numerics;
 
 namespace ProjectEuler
 {
     class Program
     {
+        // Borrows heavily from blog posts at https://www.mathblog.dk/project-euler-108-diophantine-equation/ 
+        //    and https://www.mathblog.dk/project-euler-110-efficient-diophantine-equation/
+
+        private static List<int> _primes;
+        private static int[] _exponents;
+        private static BigInteger result;
+
         static void Main(string[] args)
         {
-            BigInteger result = 1;
+            Stopwatch clock = Stopwatch.StartNew();
 
-            var primes = Helpers.GenerateIntPrimesBySieve(80000000);
+            _primes = Helpers.GenerateIntPrimesBySieve(50);
+            _exponents = new int[_primes.Count];
+
+            // Set result to product; reset when find smaller number
+            result = 1;
+            _primes.ForEach(x => result *= x);
 
             var numberToExceed = 4000000;
-            var workingValue = Convert.ToInt32(Math.Pow(Math.Ceiling(Math.Sqrt(numberToExceed * 2 - 1)), 2));
+            var upperLimit = numberToExceed * 2 - 1;
 
-            var list = Helpers.GetPrimeFactors(workingValue);
-            list.Reverse();
+            var exponentPosition = 1;
 
-            for (int i = 0; i < list.Count; i++)
+            while (true)
             {
-                result *= BigInteger.Pow(primes[i], (list[i] - 1) / 2);
+                SetTwoExponent(upperLimit);
+
+                // An exponent for a value must not be less than one for a higher value
+                if (_exponents[0] < _exponents[1])
+                {
+                    //invalid combination - move to next position 
+                    exponentPosition++;
+                }
+                else
+                {
+                    BigInteger number = GetNumber();
+                    if (number < result)
+                    {
+                        Console.WriteLine(number);
+                        result = number;
+                    }
+                    exponentPosition = 1;
+                }
+
+                if (exponentPosition >= _exponents.Length)
+                    break;
+
+                _exponents[exponentPosition]++;
+                SetAllSmallerExponents(exponentPosition);
             }
 
+            clock.Stop();
             Console.WriteLine("Answer: " + result);
+            Console.WriteLine("Solution took {0} ms", clock.Elapsed.TotalMilliseconds);
             Console.ReadLine();
+        }
+
+        // Hydrate the number, if smaller than current result
+        private static BigInteger GetNumber()
+        {
+            BigInteger number = 1;
+
+            for (int i = 0; i < _exponents.Length; i++)
+            {
+                if (_exponents[i] == 0) break;
+
+                number *= BigInteger.Pow(_primes[i], _exponents[i]);
+
+                if (number > result) return result + 1;
+            }
+
+            return number;
+        }
+
+        // Set max exponent value for 2, based on number of divisors allowed
+        private static void SetTwoExponent(long limit)
+        {
+            _exponents[0] = 0;
+            long divisors = 1;
+
+            for (int i = 0; i < _exponents.Length; i++)
+            {
+                divisors *= 2 * _exponents[i] + 1;
+            }
+            _exponents[0] = (int)(limit / divisors - 1) / 2;
+
+            while (divisors * (2 * _exponents[0] + 1) < limit)
+                _exponents[0]++;
+        }
+
+        private static void SetAllSmallerExponents(int position)
+        {
+            for (int i = 0; i < position; i++)
+            {
+                _exponents[i] = _exponents[position];
+            }
         }
     }
 }
---
+++ PE187/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // OEIS A066265		a(n) = number of semiprimes < 10^n.
+            // 0, 3, 34, 299, 2625, 23378, 210035, 1904324, 17427258
+
+
+            Stopwatch clock = Stopwatch.StartNew();
+            long result = 0;
+
+            var topNumber = 100000000;
+
+            Console.WriteLine("Getting primes...");
+            var primes = Helpers.GeneratePrimeSieve(topNumber/2);
+
+            for (var i = 1; i <= topNumber / i; i++)
+            {
+                if (!primes[i]) continue;
+                for (var j = i; j <= topNumber / i; j++)
+                {
+                    if (!primes[j]) continue;
+                    result++;
+                }
+            }
+
+            clock.Stop();
+            Console.WriteLine("Answer: " + result);
+            Console.WriteLine("Solution took {0} ms", clock.Elapsed.TotalMilliseconds);
+            Console.ReadLine();
+        }
+    }
+}
