ba66824c9bd710fc61483b313c4a226b5f2bf2ee
Berk Isaac <bisaac@geico.com>
2018-11-21 07:52:56 -0500

    Start PE 336

--- PE128/Program.cs
+++ PE128/Program.cs
@@ -20,22 +20,18 @@ namespace ProjectEuler
 
                 // check "end" of ring
                 long endValue = 6 * layer * (layer + 1) / 2 + 1;
-                //if (primes[6 * (layer + 2) * (layer + 1) / 2 - endValue] && primes[endValue - (6 * layer * (layer - 1) / 2 + 2)] && primes[endValue - (6 * (layer - 1) * (layer - 2) / 2 + 2)])
                 if (IsPrime(6 * (layer + 2) * (layer + 1) / 2 - endValue) && IsPrime(endValue - (6 * layer * (layer - 1) / 2 + 2)) && IsPrime(endValue - (6 * (layer - 1) * (layer - 2) / 2 + 2)))
                 {
                     found++;
-                    //Console.WriteLine("{4}-{0} ==> {1},{2},{3}", endValue, 6 * (layer + 2) * (layer + 1) / 2, 6 * layer * (layer - 1) / 2 + 2, 6 * (layer - 1) * (layer - 2) / 2 + 2, found);
                     result = endValue;
                     if (found == needed) break;
                 }
 
                 // check "start" of next ring
                 long startValue = endValue + 1;
-                // if (found != needed && primes[6 * (layer + 2) * (layer + 1) / 2 + 1 - startValue] && primes[6 * (layer + 2) * (layer + 1) / 2 + 3 - startValue] && primes[6 * (layer + 2) * (layer + 3) / 2 + 1 - startValue])
                 if (IsPrime(6 * (layer + 2) * (layer + 1) / 2 + 1 - startValue) && IsPrime(6 * (layer + 2) * (layer + 1) / 2 + 3 - startValue) && IsPrime(6 * (layer + 2) * (layer + 3) / 2 + 1 - startValue))
                 {
                     found++;
-                    //Console.WriteLine("{4}-{0} ==> {1},{2},{3}", startValue, 6 * (layer + 2) * (layer + 1) / 2 + 1, 6 * (layer + 2) * (layer + 1) / 2 + 3, 6 * (layer + 2) * (layer + 3) / 2 + 1,found);
                     result = startValue;
                 }
 
---
+++ PE336/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch clock = Stopwatch.StartNew();
+            string result = String.Empty;
+
+            Dictionary<string, int> combinations = new Dictionary<string, int>();
+            combinations.Add("ABCDEF", 0);
+            var spins = 0;
+
+            while (true)
+            {
+                var currentCombos = combinations.Where(x => x.Value == spins).ToList();
+                if (currentCombos.Count == 0) break;
+                foreach (var combo in currentCombos)
+                {
+                    for (var i = 2; i <= combo.Key.Length; i++)
+                    {
+                        var newCombo = RotateStringEnd(combo.Key, i);
+                        if (newCombo == "DFAECB") Console.WriteLine("DFAECB ==> " + spins + 1);
+                        if (!combinations.ContainsKey(newCombo)) combinations.Add(newCombo, spins + 1);
+                    }
+                }
+
+                spins++;
+            }
+
+            var topCombos = combinations.Where(x => x.Value == spins - 1).Select(x => x.Key).ToList();
+            topCombos.Sort();
+
+            clock.Stop();
+            Console.WriteLine("Answer: " + result);
+            Console.WriteLine("Solution took {0} ms", clock.Elapsed.TotalMilliseconds);
+            Console.ReadLine();
+        }
+
+        private static string RotateStringEnd(string start, int numLetters)
+        {
+            var sb = new StringBuilder();
+
+            if (numLetters < start.Length)
+            {
+                sb.Append(start.Substring(0, start.Length - numLetters));
+            }
+
+            for (var i = 1; i <= numLetters; i++)
+            {
+                sb.Append(start[start.Length - i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
