1f593733728ed5272d09a738f13137151d7357d0
Berk Isaac <berk.isaac@gmail.com>
2018-12-05 05:46:18 -0500

    Temporary PE129

--- PEBlank/Program.cs
+++ PEBlank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Numerics;
 
 namespace ProjectEuler
 {
@@ -10,11 +11,35 @@ namespace ProjectEuler
             Stopwatch clock = Stopwatch.StartNew();
             long result = 0;
 
+            for (int i = 1; i < 100; i += 2)
+            {
+                if (Gcd(i, 10) != 1) continue;
+                Console.WriteLine("{0} - {1}", i, BuildRepUnit(i));
+            }
+
+            result = BuildRepUnit(41);
 
             clock.Stop();
             Console.WriteLine("Answer: " + result);
             Console.WriteLine("Solution took {0} ms", clock.Elapsed.TotalMilliseconds);
             Console.ReadLine();
+        }
+
+        private static int BuildRepUnit(int divisor)
+        {
+            int size = 1;
+            BigInteger repUnit = 1;
+            while (repUnit % divisor != 0)
+            {
+                size++;
+                repUnit = (10 * repUnit) + 1;
+            }
+            return size;
+        }
+
+        private static int Gcd(int x, int y)
+        {
+            return y == 0 ? x : Gcd(y, x % y);
         }
     }
 }
