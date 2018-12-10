eb230fd7eedb1200ec1332a359c356cc536849cd
Berk Isaac <bisaac@geico.com>
2018-12-10 15:34:36 -0500

    Start PE134

---
+++ PE134/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch clock = Stopwatch.StartNew();
+            long result = 0;
+
+            var primes = Helpers.GenerateIntPrimesBySieve(1000000);
+            primes.Remove(2);
+            primes.Remove(3);
+
+
+            clock.Stop();
+            Console.WriteLine("Answer: " + result);
+            Console.WriteLine("Solution took {0} ms", clock.Elapsed.TotalMilliseconds);
+            Console.ReadLine();
+        }
+    }
+}
