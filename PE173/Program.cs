18fa9801994cdb1076069454150d869e38473be0
Berk Isaac <berk.isaac@gmail.com>
2018-12-29 18:13:27 -0500

    PE173

---
+++ PE173/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch clock = Stopwatch.StartNew();
+            long result = 0;
+            int upperLimit = 1000000;
+
+            for (long o = (upperLimit / 4) + 1; o >= 3; o--)
+                for (long i = o - 2; i >= 1; i -= 2)
+                    if (o * o - i * i <= upperLimit) result++;
+
+            clock.Stop();
+            Console.WriteLine("Answer: " + result);
+            Console.WriteLine("Solution took {0} ms", clock.Elapsed.TotalMilliseconds);
+            Console.ReadLine();
+        }
+    }
+}
