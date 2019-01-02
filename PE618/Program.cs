098489d5d73a9017d425170bd03b2b6154780ba5
Berk Isaac <bisaac@geico.com>
2019-01-02 14:52:42 -0500

    Realized mistake; need to rework PE618

--- PE618/Program.cs
+++ PE618/Program.cs
@@ -19,15 +19,23 @@ namespace ProjectEuler
 
             var term = new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368 };
 
-            primes = new List<int>();
+            // primes = new List<int>();
+            primes = Helpers.GenerateIntPrimesBySieve(term[finalTerm]);
             values = new List<int>[46369];
             values[1] = new List<int>();
 
-            for (int i = 2; i <= term[finalTerm]; i++)
+            //for (int i = 2; i <= term[finalTerm]; i++)
+            for (int i = 2; i <= finalTerm; i++)
             {
-                if (Helpers.IsPrime(i)) primes.Add(i);
+                //if (Helpers.IsPrime(i))
+                //{
+                //    primes.Add(i);
+                //    values[i] = new List<int> {i};
+                //}
 
-                FindEntries(i, 0, 0, 1);
+                // FindEntries(i);
+                Console.WriteLine($"{i,-8} : {term[i]}");
+                FindEntries(term[i], 0, 0, 1);
             }
 
             //result = term.Length;
@@ -38,6 +46,21 @@ namespace ProjectEuler
             Console.WriteLine("Answer: " + result);
             Console.WriteLine("Solution took {0} ms", clock.Elapsed.TotalMilliseconds);
             Console.ReadLine();
+        }
+
+        private static void FindEntries(int value)
+        {
+            if (values[value] == null) values[value] = new List<int>();
+
+            for (var p = 0; p < primes.Count && primes[p] < value; p++)
+            {
+                var diff = value - primes[p];
+                foreach (var product in values[diff])
+                {
+                    var currentProduct = (product * primes[p]) % 1000000;
+                    if (!values[value].Contains(currentProduct)) values[value].Add(currentProduct);
+                }
+            }
         }
 
         private static void FindEntries(int totalSum, int startingPrime, int currentSum, int currentProduct)
