508031e613c38534f3570b187f9aa8e30304788b
Berk Isaac <bisaac@geico.com>
2019-01-24 15:35:19 -0500

    Added PE480

--- PE480/Program.cs
+++ PE480/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Numerics;
+using Combinatorics.Collections;
 
 namespace ProjectEuler
 {
@@ -15,52 +17,71 @@ namespace ProjectEuler
             //var phrase = "thereisasyetinsufficientdataforameaningfulanswer";
             var phrase = "aaaaaacdeeeeeeffffghiiiiilmnnnnnorrrssssttttuuwy";
 
-            BigInteger subcount = 1;
-            for (var count = 1; count <= 15; count++)
+            //BigInteger subcount = 1;
+            //for (var count = 1; count <= 15; count++)
+            //{
+            //    for (var i = 2; i <= phrase.Length; i++)
+            //        subcount *= i;
+            //    //for (var i = 2; i <= count; i++)
+            //    //    subcount /= i;
+            //    for (var i = 2; i <= phrase.Length - count; i++)
+            //        subcount /= i;
+
+            //    // a
+            //    for (var i = 2; i <= 6; i++)
+            //        subcount /= i;
+            //    // e
+            //    for (var i = 2; i <= 6; i++)
+            //        subcount /= i;
+            //    // f
+            //    for (var i = 2; i <= 4; i++)
+            //        subcount /= i; ;
+            //    // i
+            //    for (var i = 2; i <= 5; i++)
+            //        subcount /= i;
+            //    // n
+            //    for (var i = 2; i <= 5; i++)
+            //        subcount /= i;
+            //    // r
+            //    for (var i = 2; i <= 3; i++)
+            //        subcount /= i; ;
+            //    // s
+            //    for (var i = 2; i <= 4; i++)
+            //        subcount /= i; ;
+            //    // t
+            //    for (var i = 2; i <= 4; i++)
+            //        subcount /= i; ;
+            //    // u
+            //    for (var i = 2; i <= 2; i++)
+            //        subcount /= i; ;
+
+            //    result += subcount;
+            //}
+
+            //var testPhrase = "aabc";
+            var inputSet = phrase.ToCharArray();
+
+            //Combinations<char> variations = new Combinations<char>(inputSet, 5, GenerateOption.WithoutRepetition);
+            //Console.WriteLine($"Variations of {{phrase}} choose 5: size = {variations.Count}");
+            //foreach (IList<char> v in variations)
+            //{
+            //    Console.Write("{{");
+            //    for (var k = 0; k < v.Count; k++)
+            //        Console.Write(v[k]);
+            //    Console.WriteLine("}}");
+            //}
+
+            for (var p = 1; p <= phrase.Length; p++)
             {
-                for (var i = 2; i <= phrase.Length; i++)
-                    subcount *= i;
-                //for (var i = 2; i <= count; i++)
-                //    subcount /= i;
-                for (var i = 2; i <= phrase.Length - count; i++)
-                    subcount /= i;
-
-                // a
-                for (var i = 2; i <= 6; i++)
-                    subcount /= i;
-                // e
-                for (var i = 2; i <= 6; i++)
-                    subcount /= i;
-                // f
-                for (var i = 2; i <= 4; i++)
-                    subcount /= i; ;
-                // i
-                for (var i = 2; i <= 5; i++)
-                    subcount /= i;
-                // n
-                for (var i = 2; i <= 5; i++)
-                    subcount /= i;
-                // r
-                for (var i = 2; i <= 3; i++)
-                    subcount /= i; ;
-                // s
-                for (var i = 2; i <= 4; i++)
-                    subcount /= i; ;
-                // t
-                for (var i = 2; i <= 4; i++)
-                    subcount /= i; ;
-                // u
-                for (var i = 2; i <= 2; i++)
-                    subcount /= i; ;
-
-                result += subcount;
+                Combinations<char> variations = new Combinations<char>(inputSet, p, GenerateOption.WithoutRepetition);
+                Console.WriteLine($"Variations of {{phrase}} choose {p}: size = {variations.Count}");
             }
 
             // 525069350231428029
 
             clock.Stop();
-            Console.WriteLine("Is it?: 525069350231428029");
-            Console.WriteLine("Answer: " + result);
+            //Console.WriteLine("Is it?: 525069350231428029");
+            //Console.WriteLine("Answer: " + result);
             Console.WriteLine("Solution took {0} ms", clock.Elapsed.TotalMilliseconds);
             Console.ReadLine();
         }
