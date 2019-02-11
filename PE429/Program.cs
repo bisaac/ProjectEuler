4d798fb014785ad2630ff6aec1e9c6679a1c2029
Berk Isaac <bisaac@geico.com>
2019-02-11 15:06:59 -0500

    Start PE429

---
+++ PE429/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch clock = Stopwatch.StartNew();
+            long result = 0;
+            int upperLimit = 4; // 100000000; //
+
+            var primes = Helpers.GenerateIntPrimesBySieve(upperLimit);
+            var primeCounts = new int[primes.Count];
+
+            for (var i = 0; i < primes.Count; i++)
+            {
+                var step = primes[i];
+                while (step <= upperLimit)
+                {
+                    primeCounts[i] += upperLimit / step;
+                    step *= primes[i];
+                }
+            }
+
+            clock.Stop();
+            Console.WriteLine("Answer: " + result);
+            Console.WriteLine("Solution took {0} ms", clock.Elapsed.TotalMilliseconds);
+            Console.ReadLine();
+        }
+    }
+}
