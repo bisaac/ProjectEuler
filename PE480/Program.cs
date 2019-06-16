61e9410ad5bacedd80ce29ebec6187ca82237e4d
Berk Isaac <berk.isaac@gmail.com>
2019-06-15 20:03:14 -0400

    Trying to make progress

--- PE480/Program.cs
+++ PE480/Program.cs
@@ -9,89 +9,60 @@ namespace ProjectEuler
 {
     class Program
     {
+        static private BigInteger result = 0;
+
         static void Main(string[] args)
         {
             Stopwatch clock = Stopwatch.StartNew();
-            BigInteger result = 0;
 
-            //var phrase = "thereisasyetinsufficientdataforameaningfulanswer";
-            //var phrase = "aaaaaacdeeeeeeffffghiiiiilmnnnnnorrrssssttttuuwy";
-            var phrase = "aabbbcd";
+            // var phrase = "thereisasyetinsufficientdataforameaningfulanswer";
+            // var phrase = "aaaaaacdeeeeeeffffghiiiiilmnnnnnorrrssssttttuuwy";
+            var phraseLetters = new char[] { 'a', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'l', 'm', 'n', 'o', 'r', 's', 't', 'u', 'w', 'y' };
+            //var letterCounts = new int[] { 6, 1, 1, 6, 4, 1, 1, 5, 1, 1, 5, 1, 3, 4, 4, 2, 1, 1 };
+            var letterCounts = new int[] { 5, 1, 1, 6, 4, 1, 1, 5, 1, 1, 5, 1, 3, 4, 4, 2, 1, 1 };
+            //var phrase = "aabbbcd";
+            //var phraseLetters = new char[] { 'a', 'b', 'c', 'd' };
+            //var letterCounts = new int[] { 2, 3, 1, 1 };
 
-            //BigInteger subcount = 1;
-            //for (var count = 1; count <= 15; count++)
-            //{
-            //    for (var i = 2; i <= phrase.Length; i++)
-            //        subcount *= i;
-            //    //for (var i = 2; i <= count; i++)
-            //    //    subcount /= i;
-            //    for (var i = 2; i <= phrase.Length - count; i++)
-            //        subcount /= i;
-
-            //    // a
-            //    for (var i = 2; i <= 6; i++)
-            //        subcount /= i;
-            //    // e
-            //    for (var i = 2; i <= 6; i++)
-            //        subcount /= i;
-            //    // f
-            //    for (var i = 2; i <= 4; i++)
-            //        subcount /= i; ;
-            //    // i
-            //    for (var i = 2; i <= 5; i++)
-            //        subcount /= i;
-            //    // n
-            //    for (var i = 2; i <= 5; i++)
-            //        subcount /= i;
-            //    // r
-            //    for (var i = 2; i <= 3; i++)
-            //        subcount /= i; ;
-            //    // s
-            //    for (var i = 2; i <= 4; i++)
-            //        subcount /= i; ;
-            //    // t
-            //    for (var i = 2; i <= 4; i++)
-            //        subcount /= i; ;
-            //    // u
-            //    for (var i = 2; i <= 2; i++)
-            //        subcount /= i; ;
-
-            //    result += subcount;
-            //}
-
-            //var testPhrase = "aabc";
-            var inputSet = phrase.ToCharArray();
-
-            //Combinations<char> variations = new Combinations<char>(inputSet, 5, GenerateOption.WithoutRepetition);
-            //Console.WriteLine($"Variations of {{phrase}} choose 5: size = {variations.Count}");
-            //foreach (IList<char> v in variations)
-            //{
-            //    Console.Write("{{");
-            //    for (var k = 0; k < v.Count; k++)
-            //        Console.Write(v[k]);
-            //    Console.WriteLine("}}");
-            //}
-
-            ////for (var p = 1; p <= phrase.Length; p++)
-            //for (var p = 1; p <= 15; p++)
+            //var inputSet = phrase.ToCharArray();
+            //for (var p = 1; p <= phrase.Length; p++)
             //{
             //    Variations<char> variations = new Variations<char>(inputSet, p, GenerateOption.WithoutRepetition);
             //    Console.WriteLine($"Variations of {{phrase}} choose {p}: size = {variations.Count}");
             //    result += variations.Count;
             //}
 
-            for (var p = 1; p <= phrase.Length; p++)
-            {
-                Variations<char> variations = new Variations<char>(inputSet, p, GenerateOption.WithoutRepetition);
-                Console.WriteLine($"Variations of {{phrase}} choose {p}: size = {variations.Count}");
-                result += variations.Count;
-            }
+            result++;
+            FindCountForLength(14, letterCounts);
 
             clock.Stop();
             Console.WriteLine("Is it   525069350231428029");
             Console.WriteLine("Answer: " + result);
             Console.WriteLine("Solution took {0} ms", clock.Elapsed.TotalMilliseconds);
             Console.ReadLine();
+        }
+
+        static void FindCountForLength(int Length, int[] counts)
+        {
+            //if (Length == 1)
+            //{
+            //    result += counts.Count(c => c > 0);
+            //    return;
+            //}
+
+            for (var i = 0; i < counts.Length; i++)
+            {
+                if (counts[i] != 0)
+                {
+                    result++;
+                    if (Length > 1)
+                    {
+                        counts[i]--;
+                        FindCountForLength(Length - 1, counts);
+                        counts[i]++;
+                    }
+                }
+            }
         }
     }
 }
