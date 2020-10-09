2083992cb7eac8f84c9a146a63d9d786f5eab30d
Berk Isaac <bisaac@geico.com>
2020-10-09 10:11:35 -0400

    Remove commented lines

--- PE686/Program.cs
+++ PE686/Program.cs
@@ -9,35 +9,29 @@ namespace ProjectEuler
         {
             Stopwatch clock = Stopwatch.StartNew();
 
-            var startingValue = (decimal)Math.Log(123, 2);
-            var endingValue = (decimal)Math.Log(124, 2);
-            var log10 = (decimal)Math.Log(10, 2);
+            var startingValue = Math.Log(1.23, 10);
+            var endingValue = Math.Log(1.24, 10);
+            var log2 = Math.Log(2, 10);
 
-            int desiredNumber = 45; // 678910;
+            int desiredNumber = 678910;
             int currentNumber = 0;
+            int i = 0;
 
             while(currentNumber < desiredNumber)
             {
-                startingValue += log10;
-                endingValue += log10;
+                i++;
+                var currentValue = log2 * i;
+                currentValue -= Math.Floor(currentValue);
 
-                var floorStart = Math.Floor(startingValue);
-                var floorEnd = Math.Floor(endingValue);
-
-                // Console.WriteLine($"Start: {floorEnd:0}   End: {floorEnd:0}  CurrentNumber: {currentNumber}");
-
-                if (floorEnd > floorStart)
+                if (currentValue >= startingValue && currentValue < endingValue)
                 {
                     currentNumber++;
-                    // Console.WriteLine($"Start: {floorStart:0}   End: {floorEnd:0}  CurrentNumber: {currentNumber}");
-                    if (currentNumber == desiredNumber)
-                    {
-                        clock.Stop();
-                        Console.WriteLine($"Answer: {floorEnd:0}");
-                        Console.WriteLine("Solution took {0} ms", clock.Elapsed.TotalMilliseconds);
-                    }
                 }
             }
+
+            clock.Stop();
+            Console.WriteLine($"Answer: {i:0}");
+            Console.WriteLine("Solution took {0} ms", clock.Elapsed.TotalMilliseconds);
 
             Console.ReadLine();
         }
